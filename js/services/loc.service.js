60dabeb20e7800a8375dda5d230a7a946ea30431
Lihi Ben Shimol <lihibs26@gmail.com>
2022-12-21 18:38:31 +0200

    notes,util-service, async-storage

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,32 +1,26 @@
+import { storageService } from "./async-storage.service.js"
+import { utilService } from './util.service.js'
+
+const LOC_KEY = 'locsDB'
+
 export const locService = {
     getLocs,
-    addLocation
 }
 
 
 const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
-    { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
+    { name: 'Greatplace', id: 1, lat: 32.047104, lng: 34.832384 },
+    { name: 'Neveragain', id: 2, lat: 32.047201, lng: 34.832581 }
 ]
 
 function getLocs() {
+    //todo check if locs in local storage if not return loce
+    
+
+
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve(locs)
         }, 2000)
     })
-}
-
-function addLocation() {
-    locs.push(_createLocation)
-}
-
-function _createLocation(name, lat, lng) {
-    return {
-        id: makeId(),
-        lat,
-        lng,
-        name,
-        createdAt: Date.now()
-    }
 }
